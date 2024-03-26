9e2d5bf5f49f0a6213953188e7d591619fc9674e
sanketvishwasrao111@gmail.com <sanketvishwasrao111@gmail.com>
2024-03-26 15:56:30 +0530

    Practice

--- p12.py
+++ p12.py
@@ -2,10 +2,46 @@
 # Define an Area() method of the class which calculates the area of the circle.
 # Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle.
 
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
 
+    def area(self):
+        return (22/7) * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * (22/7) * self.radius
 
-
+c1 = Circle(21)
+print(c1.area())
+print(c1.perimeter())
 
 
 
 # Define an Employee class with attributes role, department 
+# and salary. This class also has the showDetails() method.
+# Create an Engineer class that inherits properties from Employee & has additional
+# attributes : name & age.
+
+class Employee:
+    def __init__(self, role, dept, salary):
+        self.role = role
+        self.dept = dept
+        self.salary = salary
+
+    def showDetails(self):
+        print("role=", self.role)
+        print("department=", self.dept)
+        print("salary=", self.salary)
+
+e1 = Employee("Accountant", "Finance", "60,000")
+e1.showDetails()
+
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Engineer", "IT", "75,000")
+
+engg1 = Engineer("Sanket","25")
+engg1.showDetails()
